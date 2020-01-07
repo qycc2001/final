9efc6cdaa0f735b30a4c0e328c456fff9c701955
qycc2001 <1121702887@qq.com>
2020-01-06 23:42:08 -0800

    ex1.c

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    double a,b,c;
+    printf("bring in a b:");
+    scanf("%d %d",&a,&b);
+    c = (a + b) * (a + b);
+    printf("(a + b) * (a + b):%d\n",c);
+   }
